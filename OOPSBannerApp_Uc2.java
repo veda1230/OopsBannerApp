ad5673c53f5ec9595b26f312c65ae631fb3d5cb4
veda1230 <vv1576@srmist.edhu.in>
2026-03-02 14:02:06 +0530

    add Uc2

---
+++ OOPSBannerApp_Uc2.java
@@ -0,0 +1,13 @@
+public class OOPSBannerApp_Uc2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***       ***     ******    ***** ");
+        System.out.println(" **   **  **    **  **   **  **     ");
+        System.out.println("**     ** **     ** **   **  **     ");
+        System.out.println("**     ** **     ** ******    ***** ");
+        System.out.println("**     ** **     ** **             **");
+        System.out.println(" **   **  **    **  **              **");
+        System.out.println("   ***       ***     **        *****  ");
+    }
+}
